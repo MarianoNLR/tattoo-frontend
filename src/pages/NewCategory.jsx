0659fe370783426880b92e331bafcb75740465b5
Mariano Lotero Rolón <marianonehuenlr@gmail.com>
2024-04-19 17:55:07 -0300

    no access when token invalid

--- src/pages/NewCategory.jsx
+++ src/pages/NewCategory.jsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
 import NewCategoryForm from '../components/NewCategoryForm.jsx'
 import './styles/newCategory.css'
 
 export default function NewCategory () {
+  const navigate = useNavigate()
+  useEffect(() => {
+    const token = localStorage.getItem('loggedTattooApp')
+
+    if (!token) navigate('/login')
+  }, [])
+
+  
 
   return(
     <>
--- src/pages/NewTattoo.jsx
+++ src/pages/NewTattoo.jsx
@@ -1,9 +1,20 @@
+import { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
 import NewTattooForm from "../components/NewTattooForm.jsx"
 import Header from '../components/Header.jsx'
 import Footer from '../components/Footer.jsx'
 import './styles/newTattoo.css'
 
 export default function NewTattoo () {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+    const token = localStorage.getItem('loggedTattooApp')
+
+    if (!token) {
+      navigate('/login')
+    }
+  }, [])
   return (
     <>
       <Header></Header>
